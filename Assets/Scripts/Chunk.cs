3284d8a57288978463ee702fb874ffefcfb3f1d1
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-10-12 01:32:25 +0300

    First chunk

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -7,40 +7,129 @@ public class Chunk : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
+    private int vertexIndex = 0;
+    // all vertices in the chunk
+    private List<Vector3> vertices = new();
+
+    // all triangles in the chunk
+    private List<int> triangles = new();
+
+    // all uvs in the chunk
+    private List<Vector2> uvs = new();
+
+
+    // a 3d array of booleans that represents the voxel map (solid/transparent)
+    bool[,,] voxelMap = new bool[VoxelData.chunkWidth, VoxelData.chunkHeight, VoxelData.chunkWidth];
+
     private void Start()
     {
-        int vertexIndex = 0;
-        // all vertices in the chunk
-        List<Vector3> vertices = new();
+        PopulateVoxelMap();
+        CreateMeshData();
+        CreateMesh();
+    }
 
-        // all triangles in the chunk
-        List<int> triangles = new();
 
-        // all uvs in the chunk
-        List<Vector2> uvs = new();
+    // create the mesh data for the chunk
+    private void CreateMeshData()
+    {
+        // loop through all voxels in the chunk
+        for (int y = 0; y < VoxelData.chunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.chunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.chunkWidth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
+            }
+        }
+    }
 
+    
+
+    // populate the voxel map with all solid voxels
+    private void PopulateVoxelMap()
+    {
+        // loop through all voxels in the chunk
+        for (int y = 0; y < VoxelData.chunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.chunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.chunkWidth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
+            }
+        }
+    }
+    private bool CheckVoxel(Vector3 pos)
+    {
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if (x < 0 || x > VoxelData.chunkWidth -1 || y < 0 || y > VoxelData.chunkHeight - 1 || z < 0 || z > VoxelData.chunkWidth - 1)
+        {
+            return false;
+        }
+
+        return voxelMap[x, y, z];
+    }
+
+    // add vertexes , triangles, and uvs to the chunk
+    private void AddVoxelDataToChunk(Vector3 pos)
+    {
         // loop through all faces in the voxel cube
         for (int p = 0; p < 6; p++)
         {
-            // loop through all vertices in the face
-            for (int i = 0; i < 6; i++)
+            // check if parent is transparent
+            if (!CheckVoxel(VoxelData.faceChecks[p] + pos))
             {
-                // get the index of the vertex in voxelVerts
-                int triangleIndex = VoxelData.voxelTris[p, i];
+                vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, 0]] + pos);
+                vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, 1]] + pos);
+                vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, 2]] + pos);
+                vertices.Add(VoxelData.voxelVerts[VoxelData.voxelTris[p, 3]] + pos);
 
-                // add the vertex to the list of vertices
-                vertices.Add(VoxelData.voxelVerts[triangleIndex]);
+                uvs.Add(VoxelData.voxelUvs[0]);
+                uvs.Add(VoxelData.voxelUvs[1]);
+                uvs.Add(VoxelData.voxelUvs[2]);
+                uvs.Add(VoxelData.voxelUvs[3]);
 
-                // add the index of the vertex to the list of triangles to keep track of the order
                 triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
 
-                // add the uv to the list of uvs
-                uvs.Add(VoxelData.voxelUvs[i]);
 
-                vertexIndex++;
+                vertexIndex += 4;
             }
         }
+    }
+    /*
+     // loop through all vertices in the face
+                for (int i = 0; i < 6; i++)
+                {
+                    // get the index of the vertex in voxelVerts
+                    int triangleIndex = VoxelData.voxelTris[p, i];
 
+                    // add the vertex to the list of vertices
+                    vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+
+                    // add the index of the vertex to the list of triangles to keep track of the order
+                    triangles.Add(vertexIndex);
+
+                    // add the uv to the list of uvs
+                    uvs.Add(VoxelData.voxelUvs[i]);
+
+                    vertexIndex++;
+                }
+     
+     */
+    // create a mesh from the vertices, triangles, and uvs
+    private void CreateMesh()
+    {
         // create a new mesh
         Mesh mesh = new()
         {
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public static class VoxelData
 {
+    public static readonly int chunkWidth = 5;
+    public static readonly int chunkHeight = 5;
+
     // lookup list of all vertexes in a voxel cube
     // each Vector3 is a vertex 
     public static readonly Vector3[] voxelVerts = new Vector3[8]
@@ -18,25 +21,33 @@ public static class VoxelData
         new Vector3(0.0f,1.0f,1.0f),
     };
 
-    // lookup list of all faces in a voxel cube
-    // each sublist contains the vertexes from voxelVerts that make up a face
-    public static readonly int[,] voxelTris = new int[6, 6]
+    public static readonly Vector3[] faceChecks = new Vector3[6]
     {
-        {0, 3, 1, 1, 3, 2}, // back face
-        {5, 6, 4, 4, 6, 7}, // front face
-        {3, 7, 2, 2, 7, 6}, // top face
-        {1, 5, 0, 0, 5, 4}, // bottom face
-        {4, 7, 0, 0, 7, 3}, // left face
-        {1, 2, 5, 5, 2, 6}  // right face
+        new Vector3( 0.0f, 0.0f,-1.0f),   // check back face
+        new Vector3( 0.0f, 0.0f, 1.0f),    // check front face
+        new Vector3( 0.0f, 1.0f, 0.0f),    // check top face
+        new Vector3( 0.0f,-1.0f, 0.0f),   // check bottom face
+        new Vector3(-1.0f, 0.0f, 0.0f),   // check left face
+        new Vector3( 1.0f, 0.0f, 0.0f)     // check right face
     };
 
-    public static readonly Vector2[] voxelUvs = new Vector2[6]
+    // lookup list of all faces in a voxel cube
+    // each sublist contains the vertexes from voxelVerts that make up a face
+    public static readonly int[,] voxelTris = new int[6, 4]
+    {
+        {0, 3, 1, 2}, // back face
+        {5, 6, 4, 7}, // front face
+        {3, 7, 2, 6}, // top face
+        {1, 5, 0, 4}, // bottom face
+        {4, 7, 0, 3}, // left face
+        {1, 2, 5, 6}  // right face
+    };
+
+    public static readonly Vector2[] voxelUvs = new Vector2[4]
     {
         new Vector2(0.0f, 0.0f),
         new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 0.0f),
-        new Vector2(1.0f, 0.0f),
-        new Vector2(0.0f, 1.0f),
         new Vector2(1.0f, 1.0f)
     };
     
