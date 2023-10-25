5cee1522bdef7d40b6195312c49c5cbd3041c62f
GiannisKatsaros <katsaros97giannis@gmail.com>
2023-10-26 01:35:10 +0300

    ✨settings

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -49,6 +49,7 @@ public class World : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log("Generating world using seed: " + VoxelData.seed);
         // string jsonExport = JsonUtility.ToJson(settings, true);
         // Debug.Log(jsonExport);
         // File.WriteAllText(Application.dataPath + "/settings.cfg", jsonExport);
@@ -56,7 +57,7 @@ public class World : MonoBehaviour
         string jsonImport = File.ReadAllText(Application.dataPath + "/settings.cfg");
         settings = JsonUtility.FromJson<Settings>(jsonImport);
 
-        Random.InitState(settings.seed);
+        Random.InitState(VoxelData.seed);
 
         Shader.SetGlobalFloat("minGlobalLightLevel", VoxelData.minLightLevel);
         Shader.SetGlobalFloat("maxGlobalLightLevel", VoxelData.maxLightLevel);
@@ -464,17 +465,15 @@ public class VoxelMod
 public class Settings
 {
     [Header("Game Data")]
-    public string version;
+    public string version = "0.0.01";
 
     [Header("Performance")]
-    public int viewDistance;
-    public bool enableThreading;
-    public bool enableAnimatedChunks;
+    public int viewDistance = 8;
+    public bool enableThreading = true;
+    public bool enableAnimatedChunks = true;
 
     [Header("Controls")]
     [Range(0.1f, 10f)]
-    public float mouseSensitivity;
+    public float mouseSensitivity = 2f;
 
-    [Header("World Generation")]
-    public int seed;
 }
